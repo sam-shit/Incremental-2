7a08bbfbdf0c5f5d5b440a1f7bc5ea577804b7f4
Iamneo <techadmin@iamneo.ai>
2023-11-20 13:02:56 +0000

    Backup Commit

--- angularapp/src/app/services/team-service.service.ts
+++ angularapp/src/app/services/team-service.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ITeam } from '../model/iteam';
+import { IPlayer } from '../model/iplayer';
 
 @Injectable({
   providedIn: 'root'
@@ -18,6 +19,10 @@ export class TeamServiceService {
 
   getTeam(id : number) : Observable<ITeam> {
     return this.httpclient.get<ITeam>(this.url + '/ListTeam/' + id)
+  }
+
+  getPlayer(id : number) : Observable<IPlayer[]> {
+    return this.httpclient.get<IPlayer[]>(this.url + '/DisplayTeamPlayers/(id)?id=' + id)
   }
 
 
