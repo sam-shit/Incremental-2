5884bb144d6280db68354651801ef6fd1312cb73
Iamneo <techadmin@iamneo.ai>
2023-11-20 19:37:00 +0000

    Backup Commit

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListTeamsComponent } from './list-teams/list-teams.component';
 import { FindTeamComponent } from './find-team/find-team.component';
+import { AddPlayerComponent } from './add-player/add-player.component';
 
 const routes: Routes = [
   {path : 'listteams', component : ListTeamsComponent},
-  {path : 'listteam/:id', component : FindTeamComponent}
+  {path : 'listteam/:id', component : FindTeamComponent},
+  {path : 'addplayer', component : AddPlayerComponent}
 ];
 
 @NgModule({
